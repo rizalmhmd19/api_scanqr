9ebccc7b4da851025da83a7eefa25a48ef14f441
Rizal Mutaqin <zanxzhui@gmail.com>
2020-01-29 15:05:08 +0700

    edit database

--- db_flutter.sql
+++ db_flutter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 27, 2020 at 03:21 AM
+-- Generation Time: Jan 29, 2020 at 09:03 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.2.21
 
@@ -25,27 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_books`
---
-
-CREATE TABLE `tb_books` (
-  `id` int(11) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `bookname` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `picture` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_books`
---
-
-INSERT INTO `tb_books` (`id`, `username`, `bookname`, `date`, `picture`) VALUES
-(1, 'rizal', 'si rawing', '2020-01-03 03:51:10', '');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tb_login`
 --
 
@@ -60,18 +39,9 @@ CREATE TABLE `tb_login` (
 --
 
 INSERT INTO `tb_login` (`id`, `imei`, `login_time`) VALUES
-(84, '865607035212790', '2020-01-13 06:40:19'),
-(85, '865607035212790', '2020-01-14 02:50:54'),
-(86, '865607035212790', '2020-01-14 02:51:57'),
-(87, '865607035212790', '2020-01-14 02:56:24'),
-(88, '865607035212790', '2020-01-14 03:08:06'),
-(89, '865607035212790', '2020-01-15 03:18:51'),
-(90, '865607035212790', '2020-01-15 03:19:16'),
-(91, '865607035212790', '2020-01-15 07:16:44'),
-(92, '865607035212790', '2020-01-15 09:11:52'),
-(93, '865607035212790', '2020-01-16 03:00:00'),
-(94, '865607035212790', '2020-01-16 03:00:30'),
-(95, '865607035212790', '2020-01-16 03:01:32');
+(107, '865607035212782', '2020-01-29 07:53:21'),
+(108, '865607035212782', '2020-01-29 07:54:58'),
+(109, '865607035212782', '2020-01-29 07:58:14');
 
 -- --------------------------------------------------------
 
@@ -97,27 +67,9 @@ INSERT INTO `tb_log_mn` (`id`, `imei`, `menu`, `time`, `end_time`) VALUES
 (3, '865607035212790', 'About', '2020-01-23 09:41:31', '2020-01-23 09:41:38'),
 (4, '865607035212790', 'About', '2020-01-23 09:44:32', '2020-01-23 09:44:35'),
 (5, '865607035212790', 'About', '2020-01-23 09:47:22', '2020-01-23 09:47:47'),
-(6, '865607035212790', 'About', '2020-01-23 09:52:25', '2020-01-23 09:52:31');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_profile`
---
-
-CREATE TABLE `tb_profile` (
-  `id` int(11) NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `age` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_profile`
---
-
-INSERT INTO `tb_profile` (`id`, `name`, `email`, `age`) VALUES
-(1, 'Rizal', 'zanxzhui@gmail.com', 23);
+(6, '865607035212790', 'About', '2020-01-23 09:52:25', '2020-01-23 09:52:31'),
+(7, '865607035212782', 'Profile', '2020-01-29 07:24:52', NULL),
+(8, '865607035212782', 'About', '2020-01-29 07:25:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -141,18 +93,11 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id`, `username`, `email`, `password`, `level`, `imei`, `otp`, `is_login`) VALUES
-(1, 'rizal', 'zanxzhui@gmail.com', '$2y$10$rqZeie608P0RYRLnqZlq3OUYogAHjfHwIhH0SuEJp8BazcDglSZtu', 'admin', '865607035212790', 92779, 0),
-(2, 'mtqn', 'vatzymlbb@gmail.com', '$2y$10$rqZeie608P0RYRLnqZlq3OUYogAHjfHwIhH0SuEJp8BazcDglSZtu', 'member', '0', 0, 0);
+(1, 'rizal', 'zanxzhui@gmail.com', '$2y$10$rqZeie608P0RYRLnqZlq3OUYogAHjfHwIhH0SuEJp8BazcDglSZtu', 'admin', '865607035212782', 0, 1);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `tb_books`
---
-ALTER TABLE `tb_books`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tb_login`
@@ -167,12 +112,6 @@ ALTER TABLE `tb_log_mn`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_profile`
---
-ALTER TABLE `tb_profile`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
@@ -183,28 +122,16 @@ ALTER TABLE `tb_user`
 --
 
 --
--- AUTO_INCREMENT for table `tb_books`
---
-ALTER TABLE `tb_books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `tb_login`
 --
 ALTER TABLE `tb_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT for table `tb_log_mn`
 --
 ALTER TABLE `tb_log_mn`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `tb_profile`
---
-ALTER TABLE `tb_profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
